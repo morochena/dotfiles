d1407c0fe144ccccd16a46816498dee12100a0ba
Marcus Orochena <marcus.orochena@gmail.com>
2021-07-18 13:14:25 -0400

    neovim lua config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,19 @@
+-- Map leader to space
+vim.g.mapleader = ' ' 
+
+local fn = vim.fn
+local execute = vim.api.nvim_command
+
+require ('settings')
+
+-- Auto install packer.nvim if not already installed
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+end
+vim.cmd [[packadd packer.nvim]]
+vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile'
+
+require ('plugins')
+require ('keymappings')
+require ('config')

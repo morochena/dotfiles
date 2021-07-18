448ff661687dfbf542d5d86e55af9476d932be48
Marcus Orochena <marcus.orochena@gmail.com>
2021-07-18 13:15:40 -0400

    update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,10 +2,11 @@ source ~/.asdf/asdf.fish
 
 alias om="overmind"
 alias ber="bundle exec ruby"
+alias vim="nvim"
 
 set -x ASYNC_TS_CHECK true
-set -gx VOLTA_HOME "$HOME/.volta"
-set -gx PATH "$VOLTA_HOME/bin" $PATH
 
 starship init fish | source
 
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
